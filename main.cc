c0ac9671e67e557a9e48e52ecbe8df093ad5c743
Matthew <highrise2357@gmail.com>
2014-02-28 01:36:51 -0500

    It's late, so I actually have no idea what I did, but as far as I can tell, the code works.

--- main.cc
+++ main.cc
@@ -1,6 +1,18 @@
+#include <cstdlib>
 #include <iostream>
 #include "menu.h"
+using namespace std;
 int main() {
   Menu foo("root.xml", "foo");
-  if(!foo.RequestOrDie()) return 1;
+  switch (foo.RequestOrDie()) {
+    case 0: {
+      return EXIT_FAILURE;
+    }
+    case 1: {
+      return EXIT_SUCCESS;
+    }
+    default: {
+      return EXIT_FAILURE;
+    }
+  }
 }
